711a9201d919eef6f1e92c21118b6d36c408faa4
4NTC <antonio.collinassi@gmail.com>
2022-09-27 12:11:43 +0200

    first commit

---
+++ src/main/java/com/euris/academy2022/concordia/ConcordiaApplication.java
@@ -0,0 +1,13 @@
+package com.euris.academy2022.concordia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcordiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcordiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/euris/academy2022/concordia/ConcordiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.euris.academy2022.concordia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcordiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
