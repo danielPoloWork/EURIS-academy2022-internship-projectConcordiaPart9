780b45711d501bba9d7b041d09602241ebfeb71f
4NTC <antonio.collinassi@gmail.com>
2022-09-28 14:24:34 +0200

    Task (Model + Dto + Request + Repository)

---
+++ src/main/java/com/euris/academy2022/concordia/jpaRepositories/TaskJpaRepository.java
@@ -0,0 +1,30 @@
+package com.euris.academy2022.concordia.jpaRepositories;
+
+import com.euris.academy2022.concordia.dataPersistences.dataModels.Task;
+import com.euris.academy2022.concordia.utils.enums.TaskPriority;
+import com.euris.academy2022.concordia.utils.enums.TaskStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+
+public interface TaskJpaRepository extends JpaRepository<Task, String> {
+
+    String INSERT_INTO_TASK =
+            "INSERT INTO Task (Task.id, Task.title, Task.description, Task.priority, Task.status, Task.deadLine) "
+                    + "VALUES (:id, :title, :description, :priority, :status, :deadLine)";
+
+    @Modifying
+    @Query(value = INSERT_INTO_TASK, nativeQuery = true)
+    @Transactional
+    Integer create(
+            @Param("id") String id,
+            @Param("title") String title,
+            @Param("description") String description,
+            @Param("priority") String priority,
+            @Param("status") String status,
+            @Param("deadLine") LocalDateTime deadLine);
+}
