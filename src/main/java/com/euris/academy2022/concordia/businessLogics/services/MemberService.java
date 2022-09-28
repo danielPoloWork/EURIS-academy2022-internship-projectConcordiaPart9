bf7a79dea0ad73452bbaca4d5480b5b6ce750709
Marcelloo1994 <114481672+Marcelloo1994@users.noreply.github.com>
2022-09-28 16:34:39 +0200

    Update MemberService.java
    Modified interface MemberService

--- src/main/java/com/euris/academy2022/concordia/businessLogics/services/MemberService.java
+++ src/main/java/com/euris/academy2022/concordia/businessLogics/services/MemberService.java
@@ -1,4 +1,31 @@
 package com.euris.academy2022.concordia.businessLogics.services;
 
+import com.euris.academy2022.concordia.dataPersistences.dataModels.Member;
+import com.euris.academy2022.concordia.dataPersistences.dataTransferObjects.MemberDto;
+import com.euris.academy2022.concordia.utils.enums.MemberRole;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public interface MemberService {
+
+    Optional<MemberDto> insert(Member member);
+
+    Optional<MemberDto> update(Member member);
+
+    Boolean deleteAll();
+
+    Boolean deleteById(String id);
+
+    List<MemberDto> getAll();
+
+    Optional<MemberDto> getById(String id);
+
+    List<MemberDto> getByName(String name);
+
+    List<MemberDto> getBySurname(String surname);
+
+    List<MemberDto> getByRole(MemberRole role);
 }
