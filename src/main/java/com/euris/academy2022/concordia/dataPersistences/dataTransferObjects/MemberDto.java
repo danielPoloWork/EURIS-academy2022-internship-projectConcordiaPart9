b2c1dfb8490333a194ed7ea259a8374571c218ea
Marcelloo1994 <114481672+Marcelloo1994@users.noreply.github.com>
2022-09-28 14:19:17 +0200

    Create MemberDto.java
    Creation class MemberDto

---
+++ src/main/java/com/euris/academy2022/concordia/dataPersistences/dataTransferObjects/MemberDto.java
@@ -0,0 +1,31 @@
+package com.euris.academy2022.concordia.dataPersistences.dataTransferObjects;
+
+import com.euris.academy2022.concordia.dataPersistences.dataArchetypes.DtoArchetype;
+import com.euris.academy2022.concordia.dataPersistences.dataModels.Member;
+import com.euris.academy2022.concordia.utils.enums.MemberRole;
+import lombok.*;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class MemberDto implements DtoArchetype {
+
+    private String id;
+    private String name;
+    private String surname;
+    private MemberRole role;
+
+
+    @Override
+    public Member toModel() {
+        return Member.builder()
+                .id(this.id)
+                .name(this.name)
+                .surname(this.surname)
+                .role(this.role)
+                .build();
+    }
+}
