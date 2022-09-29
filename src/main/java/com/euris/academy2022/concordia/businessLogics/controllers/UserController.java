2d87e680f8f779b69194df44451f5a17b675a857
Daniel Polo <106583643+danielPoloWork@users.noreply.github.com>
2022-09-29 19:37:22 +0200

    Update UserController.java
    Fixed the controller to return ResponseDto

--- src/main/java/com/euris/academy2022/concordia/businessLogics/controllers/UserController.java
+++ src/main/java/com/euris/academy2022/concordia/businessLogics/controllers/UserController.java
@@ -1,14 +1,13 @@
 package com.euris.academy2022.concordia.businessLogics.controllers;
 
 import com.euris.academy2022.concordia.businessLogics.services.UserService;
-//import com.euris.academy2022.concordia.businessLogics.services.UserDetailsManagerService;
 import com.euris.academy2022.concordia.dataPersistences.dataModels.User;
 import com.euris.academy2022.concordia.dataPersistences.dataTransferObjects.requests.users.UserPostRequest;
 import com.euris.academy2022.concordia.dataPersistences.dataTransferObjects.requests.users.UserPutRequest;
-
+import com.euris.academy2022.concordia.dataPersistences.dataTransferObjects.responses.ResponseDto;
 import com.euris.academy2022.concordia.utils.enums.UserRole;
+
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,67 +23,42 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
 	private final UserService userService;
-//	private final UserDetailsManagerService userDetailsManagerService;
-
 	public UserController(UserService userService) {
 		this.userService = userService;
 	}
 
-//	public UserController(UserService userService,	UserDetailsManagerService userDetailsManagerService) {
-//		this.userService = userService;
-//		this.userDetailsManagerService = userDetailsManagerService;
-//	}
-
 	@PostMapping
-	public Optional<User> insert(@RequestBody UserPostRequest userDto) {
-		Optional<User> responseUserService = userService.insert(userDto.toModel());
-//		if (responseUserService.isPresent()) {
-//			Optional<User> responseUDMS = userDetailsManagerService.insert(responseUserService.get());
-//		}
-		return responseUserService;
+	public ResponseDto<User> insert(@RequestBody UserPostRequest userDto) {
+		return userService.insert(userDto.toModel());
 	}
 
 	@PutMapping
-	public Optional<User> update(@RequestBody UserPutRequest userDto) {
-		Optional<User> responseUserService = userService.update(userDto.toModel());
-//		if (responseUserService.isPresent()) {
-//			Optional<User> responseUDMS = userDetailsManagerService.update(responseUserService.get());
-//		}
-		return responseUserService;
+	public ResponseDto<User> update(@RequestBody UserPutRequest userDto) {
+		return userService.update(userDto.toModel());
 	}
 
 	@DeleteMapping("/{uuid}")
-	public Boolean deleteByUuid(@PathVariable String uuid) {
-		Boolean responseUserService = Boolean.FALSE;
-		if (!uuid.isEmpty()) {
-			Optional<User> uuidFound = getByUuid(uuid);
-			if (uuidFound.isPresent()) {
-				responseUserService = userService.deleteByUuid(uuid);
-//				if (responseUserService) {
-//					Boolean responseUDMS = userDetailsManagerService.deleteByUsername(uuidFound.get().getUsername());
-//				}
-			}
-		}
-		return responseUserService;
+	public ResponseDto<String> deleteByUuid(@PathVariable String uuid) {
+		return userService.deleteByUuid(uuid);
 	}
 
 	@GetMapping
-	public List<User> getAll() {
+	public ResponseDto<List<User>> getAll() {
 		return userService.getAll();
 	}
 
 	@GetMapping("/{uuid}")
-	public Optional<User> getByUuid(@PathVariable String uuid) {
+	public ResponseDto<User> getByUuid(@PathVariable String uuid) {
 		return userService.getByUuid(uuid);
 	}
 
 	@GetMapping("/role={role}")
-	public List<User> getByRole(@PathVariable UserRole role) {
+	public ResponseDto<List<User>> getByRole(@PathVariable UserRole role) {
 		return userService.getByRole(role);
 	}
 
 	@GetMapping("/username={username}")
-	public Optional<User> getByUsername(@PathVariable String username) {
+	public ResponseDto<User> getByUsername(@PathVariable String username) {
 		return userService.getByUsername(username);
 	}
 }
