981f7f65e21787cc65ea06d9b2750ed75892b0ed
Marcelloo1994 <114481672+Marcelloo1994@users.noreply.github.com>
2022-09-30 12:09:46 +0200

    Update MemberServiceImpl.java
    Modified MemberServiceImpl

--- src/main/java/com/euris/academy2022/concordia/businessLogics/impls/MemberServiceImpl.java
+++ src/main/java/com/euris/academy2022/concordia/businessLogics/impls/MemberServiceImpl.java
@@ -2,7 +2,11 @@ package com.euris.academy2022.concordia.businessLogics.impls;
 
 import com.euris.academy2022.concordia.businessLogics.services.MemberService;
 import com.euris.academy2022.concordia.dataPersistences.dataModels.Member;
+import com.euris.academy2022.concordia.dataPersistences.dataModels.User;
+import com.euris.academy2022.concordia.dataPersistences.dataTransferObjects.responses.ResponseDto;
 import com.euris.academy2022.concordia.jpaRepositories.MemberJpaRepository;
+import com.euris.academy2022.concordia.utils.enums.HttpRequestType;
+import com.euris.academy2022.concordia.utils.enums.HttpResponseType;
 import com.euris.academy2022.concordia.utils.enums.MemberRole;
 
 import java.util.List;
@@ -16,51 +20,199 @@ public class MemberServiceImpl implements MemberService {
         this.memberJpaRepository = memberJpaRepository;
     }
 
+
     @Override
-    public Optional<Member> insert(Member member) {
-        return memberJpaRepository.insert(
+    public ResponseDto<Member> insert(Member member) {
+        ResponseDto<Member> response = new ResponseDto<>();
+        Integer memberCreated = memberJpaRepository.insert(
                 member.getId(),
                 member.getName(),
                 member.getSurname(),
                 member.getRole().getLabel()
         );
+        response.setHttpRequest(HttpRequestType.POST);
+
+        if (memberCreated != 1) {
+            response.setHttpResponse(HttpResponseType.NOT_CREATED);
+            response.setCode(HttpResponseType.NOT_CREATED.getCode());
+            response.setDesc(HttpResponseType.NOT_CREATED.getDesc());
+        } else {
+            response.setHttpResponse(HttpResponseType.CREATED);
+            response.setCode(HttpResponseType.CREATED.getCode());
+            response.setDesc(HttpResponseType.CREATED.getDesc());
+            response.setBody(member);
+        }
+        return response;
+    }
+
+
+    @Override
+    public ResponseDto<Member> update(Member member) {
+        ResponseDto<Member> response = new ResponseDto<>();
+        Optional<Member> memberFound = memberJpaRepository.findById(member.getId());
+
+        if (memberFound.isEmpty()) {
+            response.setHttpRequest(HttpRequestType.GET);
+            response.setHttpResponse(HttpResponseType.NOT_FOUND);
+            response.setCode(HttpResponseType.NOT_FOUND.getCode());
+            response.setDesc(HttpResponseType.NOT_FOUND.getDesc());
+        } else {
+
+            Integer memberUpdated = memberJpaRepository.update(
+                    member.getId(),
+                    member.getName(),
+                    member.getSurname(),
+                    member.getRole().getLabel()
+            );
+
+            response.setHttpRequest(HttpRequestType.PUT);
+
+            if (memberUpdated != 1) {
+                response.setHttpResponse(HttpResponseType.NOT_UPDATED);
+                response.setCode(HttpResponseType.NOT_UPDATED.getCode());
+                response.setDesc(HttpResponseType.NOT_UPDATED.getDesc());
+            } else {
+                response.setHttpResponse(HttpResponseType.UPDATED);
+                response.setCode(HttpResponseType.UPDATED.getCode());
+                response.setDesc(HttpResponseType.UPDATED.getDesc());
+                response.setBody(member);
+            }
+        }
+        return response;
     }
 
     @Override
-    public Optional<Member> update(Member member) {
-        Optional<Member> memberFound = getById(member.getId());
+    public ResponseDto<Member> deleteById(String id) {
 
-        return memberFound.isEmpty() ? Optional.empty() : Optional.of(memberJpaRepository.save(member));
+        ResponseDto<Member> response = new ResponseDto<>();
+        Optional<Member> memberFound = memberJpaRepository.findById(id);
+
+        if (memberFound.isEmpty()) {
+            response.setHttpRequest(HttpRequestType.GET);
+            response.setHttpResponse(HttpResponseType.NOT_FOUND);
+            response.setCode(HttpResponseType.NOT_FOUND.getCode());
+        } else {
+            Integer memberDeleted = memberJpaRepository.removeById(id);
+
+            response.setHttpRequest(HttpRequestType.DELETE);
+
+            if (memberDeleted != 1) {
+                response.setHttpResponse(HttpResponseType.NOT_DELETED);
+                response.setCode(HttpResponseType.NOT_DELETED.getCode());
+                response.setDesc(HttpResponseType.NOT_DELETED.getDesc());
+            } else {
+                response.setHttpResponse(HttpResponseType.DELETED);
+                response.setCode(HttpResponseType.DELETED.getCode());
+                response.setDesc(HttpResponseType.DELETED.getDesc());
+                response.setBody(memberFound.get());
+            }
+        }
+
+        return response;
     }
 
     @Override
-    public Boolean deleteById(String id) {
-        memberJpaRepository.deleteById(id);
-        return Boolean.TRUE;
+    public ResponseDto<List<Member>> getAll() {
+        ResponseDto<List<Member>> response = new ResponseDto<>();
+        List<Member> memberListFound = memberJpaRepository.findAll();
+
+        response.setHttpRequest(HttpRequestType.GET);
+
+        if (memberListFound.isEmpty()) {
+            response.setHttpResponse(HttpResponseType.NOT_FOUND);
+            response.setCode(HttpResponseType.NOT_FOUND.getCode());
+            response.setDesc(HttpResponseType.NOT_FOUND.getDesc());
+        } else {
+            response.setHttpResponse(HttpResponseType.FOUND);
+            response.setCode(HttpResponseType.FOUND.getCode());
+            response.setDesc(HttpResponseType.FOUND.getDesc());
+            response.setBody(memberListFound);
+        }
+
+        return response;
     }
 
     @Override
-    public List<Member> getAll() {
-        return memberJpaRepository.findAll();
+    public ResponseDto<Member> getById(String id) {
+        ResponseDto<Member> response = new ResponseDto<>();
+        Optional<Member> memberFound = memberJpaRepository.findById(id);
+
+        response.setHttpRequest(HttpRequestType.GET);
+
+        if (memberFound.isEmpty()) {
+            response.setHttpResponse(HttpResponseType.NOT_FOUND);
+            response.setCode(HttpResponseType.NOT_FOUND.getCode());
+            response.setDesc(HttpResponseType.NOT_FOUND.getDesc());
+        } else {
+            response.setHttpResponse(HttpResponseType.FOUND);
+            response.setCode(HttpResponseType.FOUND.getCode());
+            response.setDesc(HttpResponseType.FOUND.getDesc());
+            response.setBody(memberFound.get());
+        }
+        return response;
     }
 
     @Override
-    public Optional<Member> getById(String id) {
-        return memberJpaRepository.findById(id);
+    public ResponseDto<List<Member>> getByName(String name) {
+        ResponseDto<List<Member>> response = new ResponseDto<>();
+        List<Member> memberListFound = memberJpaRepository.findByName(name);
+
+        response.setHttpRequest(HttpRequestType.GET);
+
+        if (memberListFound.isEmpty()) {
+            response.setHttpResponse(HttpResponseType.NOT_FOUND);
+            response.setCode(HttpResponseType.NOT_FOUND.getCode());
+            response.setDesc(HttpResponseType.NOT_FOUND.getDesc());
+        } else {
+            response.setHttpResponse(HttpResponseType.FOUND);
+            response.setCode(HttpResponseType.FOUND.getCode());
+            response.setDesc(HttpResponseType.FOUND.getDesc());
+            response.setBody(memberListFound);
+        }
+
+        return response;
+    }
+
+
+    @Override
+    public ResponseDto<List<Member>> getBySurname(String surname) {
+        ResponseDto<List<Member>> response = new ResponseDto<>();
+        List<Member> memberListFound = memberJpaRepository.findBySurname(surname);
+
+        response.setHttpRequest(HttpRequestType.GET);
+
+        if (memberListFound.isEmpty()) {
+            response.setHttpResponse(HttpResponseType.NOT_FOUND);
+            response.setCode(HttpResponseType.NOT_FOUND.getCode());
+            response.setDesc(HttpResponseType.NOT_FOUND.getDesc());
+        } else {
+            response.setHttpResponse(HttpResponseType.FOUND);
+            response.setCode(HttpResponseType.FOUND.getCode());
+            response.setDesc(HttpResponseType.FOUND.getDesc());
+            response.setBody(memberListFound);
+        }
+
+        return response;
     }
 
     @Override
-    public List<Member> getByName(String name) {
-        return memberJpaRepository.findByName(name);
-    }
+    public ResponseDto<List<Member>> getByRole(MemberRole role) {
+        ResponseDto<List<Member>> response = new ResponseDto<>();
+        List<Member> memberListFound = memberJpaRepository.findByRole(role.getLabel());
 
-    @Override
-    public List<Member> getBySurname(String surname) {
-        return memberJpaRepository.findBySurname(surname);
-    }
-    
-    @Override
-    public List<Member> getByRole(MemberRole role) {
-        return memberJpaRepository.findByRole(role.getLabel());
+        response.setHttpRequest(HttpRequestType.GET);
+
+        if (memberListFound.isEmpty()) {
+            response.setHttpResponse(HttpResponseType.NOT_FOUND);
+            response.setCode(HttpResponseType.NOT_FOUND.getCode());
+            response.setDesc(HttpResponseType.NOT_FOUND.getDesc());
+        } else {
+            response.setHttpResponse(HttpResponseType.FOUND);
+            response.setCode(HttpResponseType.FOUND.getCode());
+            response.setDesc(HttpResponseType.FOUND.getDesc());
+            response.setBody(memberListFound);
+        }
+
+        return response;
     }
 }
