ca277dcc7a46c4d7e446e8cf603793e3dfc9e9a7
Daniel Polo <106583643+danielPoloWork@users.noreply.github.com>
2022-09-30 21:31:43 +0200

    Reformat Code
    Reformat the whole file according to the current code style settings. Reformat Code

--- src/main/java/com/euris/academy2022/concordia/utils/enums/TaskPriority.java
+++ src/main/java/com/euris/academy2022/concordia/utils/enums/TaskPriority.java
@@ -6,39 +6,39 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Getter
 public enum TaskPriority {
-  HIGH(
-      "HIGH",
-      "6331a62237ccc99fc3e4b2ec",
-      "High Priority",
-      "red"),
-  EXPIRING(
-      "EXPIRING",
-      "6331a62237ccc99fc3e4b2e5",
-      "Expiring Priority",
-      "orange"),
-  MEDIUM(
-      "MEDIUM",
-      "6331a62237ccc99fc3e4b2e1",
-      "Medium Priority",
-      "yellow"),
-  LOW(
-      "LOW",
-      "6331a62237ccc99fc3e4b2eb",
-      "Low Priority",
-      "blue"),
-  DONE(
-      "DONE",
-      "6331a62237ccc99fc3e4b2e3",
-      "Done",
-      "green"),
-  ARCHIVED(
-      "ARCHIVED",
-      "6331a62237ccc99fc3e4b2ed",
-      "Archived",
-      "purple");
+    HIGH(
+            "HIGH",
+            "6331a62237ccc99fc3e4b2ec",
+            "High Priority",
+            "red"),
+    EXPIRING(
+            "EXPIRING",
+            "6331a62237ccc99fc3e4b2e5",
+            "Expiring Priority",
+            "orange"),
+    MEDIUM(
+            "MEDIUM",
+            "6331a62237ccc99fc3e4b2e1",
+            "Medium Priority",
+            "yellow"),
+    LOW(
+            "LOW",
+            "6331a62237ccc99fc3e4b2eb",
+            "Low Priority",
+            "blue"),
+    DONE(
+            "DONE",
+            "6331a62237ccc99fc3e4b2e3",
+            "Done",
+            "green"),
+    ARCHIVED(
+            "ARCHIVED",
+            "6331a62237ccc99fc3e4b2ed",
+            "Archived",
+            "purple");
 
-  private final String label;
-  private final String trelloLabelId;
-  private final String trelloName;
-  private final String trelloColor;
+    private final String label;
+    private final String trelloLabelId;
+    private final String trelloName;
+    private final String trelloColor;
 }
