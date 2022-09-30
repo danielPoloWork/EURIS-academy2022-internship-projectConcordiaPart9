31e02a121ce073031ae7c3d1a18b0ea3cd98f99d
Marcelloo1994 <114481672+Marcelloo1994@users.noreply.github.com>
2022-09-30 12:10:24 +0200

    Update MemberController.java
    Modified MemberController

--- src/main/java/com/euris/academy2022/concordia/businessLogics/controllers/MemberController.java
+++ src/main/java/com/euris/academy2022/concordia/businessLogics/controllers/MemberController.java
@@ -5,6 +5,7 @@ import com.euris.academy2022.concordia.businessLogics.services.MemberService;
 import com.euris.academy2022.concordia.dataPersistences.dataModels.Member;
 import com.euris.academy2022.concordia.dataPersistences.dataTransferObjects.requests.members.MemberPostRequest;
 import com.euris.academy2022.concordia.dataPersistences.dataTransferObjects.requests.members.MemberPutRequest;
+import com.euris.academy2022.concordia.dataPersistences.dataTransferObjects.responses.ResponseDto;
 import com.euris.academy2022.concordia.utils.enums.MemberRole;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,42 +23,42 @@ public class MemberController {
     }
 
     @PostMapping
-    public Optional<Member> insert(@RequestBody MemberPostRequest memberDto) {
+    public ResponseDto<Member> insert(@RequestBody MemberPostRequest memberDto) {
         return memberService.insert(memberDto.toModel());
     }
 
     @PutMapping
-    public Optional<Member> update(@RequestBody MemberPutRequest memberDto) {
+    public ResponseDto<Member> update(@RequestBody MemberPutRequest memberDto) {
         return memberService.update(memberDto.toModel());
     }
 
     @DeleteMapping("/{id}")
-    public Boolean deleteById(@PathVariable String id) {
+    public ResponseDto deleteById(@PathVariable String id) {
         return memberService.deleteById(id);
     }
 
     @GetMapping
-    public List<Member> getAll() {
+    public ResponseDto<List<Member>> getAll() {
         return memberService.getAll();
     }
 
     @GetMapping("/{id}")
-    public Optional<Member> getById(@PathVariable String id) {
+    public ResponseDto<Member> getById(@PathVariable String id) {
         return memberService.getById(id);
     }
 
     @GetMapping("/name={name}")
-    public List<Member> getByName(@PathVariable String name) {
+    public ResponseDto<List<Member>> getByName(@PathVariable String name) {
         return memberService.getByName(name);
     }
 
     @GetMapping("/surname={surname}")
-    public List<Member> getBySurname(@PathVariable String surname) {
+    public ResponseDto<List<Member>> getBySurname(@PathVariable String surname) {
         return memberService.getBySurname(surname);
     }
 
     @GetMapping("/role={role}")
-    public List<Member> getByRole(@PathVariable MemberRole role) {
+    public ResponseDto<List<Member>> getByRole(@PathVariable MemberRole role) {
         return memberService.getByRole(role);
     }
 }
