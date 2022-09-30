e259d755fc2ff3ffaccdc88480a37d80a30ef866
Daniel Polo <106583643+danielPoloWork@users.noreply.github.com>
2022-09-30 17:45:14 +0200

    Update MemberRole.java
    Added application roles and userDetailsManager becouse we decided to marge table User in to table Member. In this way we have just one enum to manage all roles.

--- src/main/java/com/euris/academy2022/concordia/utils/enums/MemberRole.java
+++ src/main/java/com/euris/academy2022/concordia/utils/enums/MemberRole.java
@@ -6,12 +6,19 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Getter
 public enum MemberRole {
+  /* Researcher roles */
   A1("A1","Scientific for astrophysics"),
   A2("A2","Scientific for chemistry and glaciology"),
   A3("A3","Scientific for atmospheric physics"),
   B1("B1","Qualified doctor emergency area"),
   C1("C1","Electronic engineer"),
-  C2("C2","ICT/Radio Technician");
+  C2("C2","ICT/Radio Technician"),
+
+  /* Application roles */
+  ADMIN("ADMIN","IT Admin"),
+
+  /* UserDetailsManager roles */
+  MANAGER("MANAGER","User detail manager");
 
   private final String label;
   private final String name;
