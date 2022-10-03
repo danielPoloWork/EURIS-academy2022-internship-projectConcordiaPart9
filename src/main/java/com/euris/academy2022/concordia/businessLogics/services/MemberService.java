fc1dbd21c20e223f35d4a1fb27ac9009d8b455fc
Daniel Polo <106583643+danielPoloWork@users.noreply.github.com>
2022-10-03 09:35:08 +0200

    Merged UserService into MemberService

--- src/main/java/com/euris/academy2022/concordia/businessLogics/services/MemberService.java
+++ src/main/java/com/euris/academy2022/concordia/businessLogics/services/MemberService.java
@@ -2,29 +2,23 @@ package com.euris.academy2022.concordia.businessLogics.services;
 
 import com.euris.academy2022.concordia.dataPersistences.dataModels.Member;
 import com.euris.academy2022.concordia.dataPersistences.dataTransferObjects.MemberDto;
-import com.euris.academy2022.concordia.dataPersistences.dataTransferObjects.responses.ResponseDto;
+import com.euris.academy2022.concordia.dataPersistences.dataTransferObjects.ResponseDto;
 import com.euris.academy2022.concordia.utils.enums.MemberRole;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public interface MemberService {
 
     ResponseDto<MemberDto> insert(Member member);
-
     ResponseDto<MemberDto> update(Member member);
-
-    ResponseDto<MemberDto> deleteById(String id);
-
+    ResponseDto<MemberDto> removeByUuid(String uuid);
     ResponseDto<List<MemberDto>> getAll();
-
-    ResponseDto<MemberDto> getById(String id);
-
-    ResponseDto<List<MemberDto>> getByName(String name);
-
-    ResponseDto<List<MemberDto>> getBySurname(String surname);
-
+    ResponseDto<MemberDto> getByUuid(String uuid);
+    ResponseDto<MemberDto> getByIdTrelloMember(String idTrelloMember);
+    ResponseDto<MemberDto> getByUsername(String username);
     ResponseDto<List<MemberDto>> getByRole(MemberRole role);
+    ResponseDto<List<MemberDto>> getByName(String name);
+    ResponseDto<List<MemberDto>> getBySurname(String surname);
 }
