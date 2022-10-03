006f8a1dc479099cd5a3cef7d80953c95ae9c17b
Daniel Polo <106583643+danielPoloWork@users.noreply.github.com>
2022-10-03 09:32:14 +0200

    Merged UserJpaRepository into MemberJpaRepository

--- src/main/java/com/euris/academy2022/concordia/jpaRepositories/MemberJpaRepository.java
+++ src/main/java/com/euris/academy2022/concordia/jpaRepositories/MemberJpaRepository.java
@@ -1,7 +1,7 @@
 package com.euris.academy2022.concordia.jpaRepositories;
 
 import com.euris.academy2022.concordia.dataPersistences.dataModels.Member;
-import com.euris.academy2022.concordia.dataPersistences.dataTransferObjects.MemberDto;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -10,53 +10,57 @@ import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Optional;
 
 
 @Repository
 public interface MemberJpaRepository extends JpaRepository<Member, String> {
-
-
     String INSERT_INTO_MEMBER =
-            "INSERT INTO Member (Member.id, Member.name, Member.surname, Member.role) "
-                    + "VALUES (:id, :name, :surname, :role)";
+            "INSERT INTO Member (Member.uuid, Member.idTrelloMember, Member.username, Member.password, Member.role, Member.name, Member.surname) "
+                    + "VALUES (UUID(), :idTrelloMember, :username, :password, :role, :name, :surname)";
 
     String UPDATE_MEMBER =
             "UPDATE Member "
-                    + "SET Member.name = :name, Member.surname = :surname, Member.role = :role "
-                    + "WHERE Member.id = :id";
+                    + "SET Member.idTrelloMember = :idTrelloMember, Member.username = :username, Member.password = :password, Member.role = :role, Member.name = :name, Member.surname = :surname "
+                    + "WHERE Member.uuid = :uuid";
 
     String DELETE_MEMBER =
             "DELETE FROM Member "
-                    + "WHERE Member.id = :id";
+                    + "WHERE Member.uuid = :uuid";
 
     @Modifying
     @Query(value = INSERT_INTO_MEMBER, nativeQuery = true)
     @Transactional
     Integer insert(
-            @Param("id") String id,
+            @Param("idTrelloMember") String idTrelloMember,
+            @Param("username") String username,
+            @Param("password") String password,
+            @Param("role") String role,
             @Param("name") String name,
-            @Param("surname") String surname,
-            @Param("role") String role
-    );
+            @Param("surname") String surname);
 
     @Modifying
     @Query(value = UPDATE_MEMBER, nativeQuery = true)
     @Transactional
     Integer update(
-            @Param("id") String id,
+            @Param("uuid") String uuid,
+            @Param("idTrelloMember") String idTrelloMember,
+            @Param("username") String username,
+            @Param("password") String password,
+            @Param("role") String role,
             @Param("name") String name,
-            @Param("surname") String surname,
-            @Param("role") String role
-    );
+            @Param("surname") String surname);
 
     @Modifying
     @Query(value = DELETE_MEMBER, nativeQuery = true)
     @Transactional
-    Integer removeById(
-            @Param("id") String id
-    );
-
+    Integer removeByUuid(
+            @Param("uuid") String uuid);
+    List<Member> findAll();
+    Optional<Member> findByUuid(String uuid);
+    Optional<Member> findByIdTrelloMember(String idTrelloMember);
+    Optional<Member> findByUsername(String username);
+    List<Member> findByRole(String role);
     List<Member> findByName(String name);
     List<Member> findBySurname(String surname);
-    List<Member> findByRole(String role);
 }
