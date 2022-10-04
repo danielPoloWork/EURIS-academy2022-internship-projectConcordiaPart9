69369d3a289183c1276702830321e95b7e4116db
Daniel Polo <106583643+danielPoloWork@users.noreply.github.com>
2022-10-04 09:14:08 +0200

    Update AssigneeService.java
    Revamped the class to suit changes. Now it's working with Assignee entity and not automated with spring

--- src/main/java/com/euris/academy2022/concordia/businessLogics/services/AssigneeService.java
+++ src/main/java/com/euris/academy2022/concordia/businessLogics/services/AssigneeService.java
@@ -1,10 +1,16 @@
 package com.euris.academy2022.concordia.businessLogics.services;
 
+import com.euris.academy2022.concordia.dataPersistences.dataModels.Assignee;
+import com.euris.academy2022.concordia.dataPersistences.dataTransferObjects.AssigneeDto;
 import com.euris.academy2022.concordia.dataPersistences.dataTransferObjects.ResponseDto;
-import com.euris.academy2022.concordia.dataPersistences.dataTransferObjects.requests.assignees.AssigneePostRequest;
+
+import java.util.List;
 
 public interface AssigneeService {
 
-
-    ResponseDto<Integer> assignMemberToTask(AssigneePostRequest assigneePostRequest);
+    ResponseDto<AssigneeDto> insert(Assignee assignee);
+    ResponseDto<AssigneeDto> removeByUuidMemberAndIdTask(String uuidMember, String idTask);
+    ResponseDto<List<AssigneeDto>> getAll();
+    ResponseDto<List<AssigneeDto>> getByUuidMember(String uuidMember);
+    ResponseDto<List<AssigneeDto>> getByIdTask(String idTask);
 }
