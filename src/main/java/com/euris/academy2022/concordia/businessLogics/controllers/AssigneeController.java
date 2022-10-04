8a62d3d12d2b214b556b02e303f794aa894db367
Daniel Polo <106583643+danielPoloWork@users.noreply.github.com>
2022-10-04 09:15:05 +0200

    Update AssigneeController.java
    Created insert api

--- src/main/java/com/euris/academy2022/concordia/businessLogics/controllers/AssigneeController.java
+++ src/main/java/com/euris/academy2022/concordia/businessLogics/controllers/AssigneeController.java
@@ -1,8 +1,10 @@
 package com.euris.academy2022.concordia.businessLogics.controllers;
 
 import com.euris.academy2022.concordia.businessLogics.services.AssigneeService;
+import com.euris.academy2022.concordia.dataPersistences.dataTransferObjects.AssigneeDto;
 import com.euris.academy2022.concordia.dataPersistences.dataTransferObjects.ResponseDto;
 import com.euris.academy2022.concordia.dataPersistences.dataTransferObjects.requests.assignees.AssigneePostRequest;
+import com.euris.academy2022.concordia.dataPersistences.dataTransferObjects.requests.members.MemberPostRequest;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,8 +20,13 @@ public class AssigneeController {
         this.assigneeService = assigneeService;
     }
 
+//    @PostMapping
+//    public ResponseDto<Integer> assignMemberToTask(@RequestBody AssigneePostRequest assigneePostRequest) {
+//       return assigneeService.assignMemberToTask(assigneePostRequest);
+//    }
+
     @PostMapping
-    public ResponseDto<Integer> assignMemberToTask(@RequestBody AssigneePostRequest assigneePostRequest) {
-       return assigneeService.assignMemberToTask(assigneePostRequest);
+    public ResponseDto<AssigneeDto> insert(@RequestBody AssigneePostRequest assigneeDto) {
+        return assigneeService.insert(assigneeDto.toModel());
     }
 }
