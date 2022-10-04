a0703c35b75770d6e107c2404ea271a3558d8386
Daniel Polo <106583643+danielPoloWork@users.noreply.github.com>
2022-10-04 12:36:47 +0200

    Update TaskStatus.java
    Fixed TrelloListId inside the enum

--- src/main/java/com/euris/academy2022/concordia/utils/enums/TaskStatus.java
+++ src/main/java/com/euris/academy2022/concordia/utils/enums/TaskStatus.java
@@ -8,15 +8,15 @@ import lombok.RequiredArgsConstructor;
 public enum TaskStatus {
     TO_DO(
             "TO_DO",
-            "a6227f9f991a315ca09e",
+            "6331a6227f9f991a315ca09e",
             "Tasks to do"),
     IN_PROGRESS(
             "IN_PROGRESS",
-            "c2489132c20205bac3ab",
+            "6331c2489132c20205bac3ab",
             "Tasks in progress"),
     COMPLETED(
             "COMPLETED",
-            "c24bce6fc400d92fe580",
+            "6331c24bce6fc400d92fe580",
             "Tasks completed");
 
     private final String label;
