e740a5ce724a3b57969589600f2b59da9588ce16
Daniel Polo <106583643+danielPoloWork@users.noreply.github.com>
2022-10-05 15:12:21 +0200

    Update TrelloCardDto.java
    Added annotation all args constructor and builder

--- src/main/java/com/euris/academy2022/concordia/dataPersistences/dataTransferObjects/TrelloCardDto.java
+++ src/main/java/com/euris/academy2022/concordia/dataPersistences/dataTransferObjects/TrelloCardDto.java
@@ -1,13 +1,13 @@
 package com.euris.academy2022.concordia.dataPersistences.dataTransferObjects;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @NoArgsConstructor
+@AllArgsConstructor
 @Getter
 @Setter
+@Builder
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class TrelloCardDto {
 
