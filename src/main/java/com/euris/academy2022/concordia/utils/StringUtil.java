8e945ac4df79ecf4cc2e778610a0aaf99dad061e
4NTC <antonio.collinassi@gmail.com>
2022-10-10 10:05:21 +0200

    StringUtil update

--- src/main/java/com/euris/academy2022/concordia/utils/StringUtil.java
+++ src/main/java/com/euris/academy2022/concordia/utils/StringUtil.java
@@ -1,30 +1,57 @@
 package com.euris.academy2022.concordia.utils;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringUtil {
 
-    public static String[] splitFullName(String fullName) {
+    public static List<String> splitFullName(String fullName) {
 
-        // string string string -> string string | string
+        List<String> stringList = new ArrayList<>();
 
-        String[] nameAndSurname = new String[2];
+        String regExNew = "(\\S+)";
 
-        String regEx = "(.*)(?:\\s(.*))";
-
-        Pattern pattern = Pattern.compile(regEx);
-
+        Pattern pattern = Pattern.compile(regExNew);
         Matcher matcher = pattern.matcher(fullName);
 
-        if (matcher.matches()) {
-            nameAndSurname[0] = matcher.group(1);
-            nameAndSurname[1] = matcher.group(2);
-        } else {
-            nameAndSurname[0] = fullName;
-            nameAndSurname[1] = "----";
+        while (matcher.find()) {
+            stringList.add(matcher.group());
         }
 
-        return nameAndSurname;
+        return stringList;
+    }
+
+    public static String listToString(List<String> stringList) {
+        StringBuilder result = new StringBuilder();
+        for (String str : stringList) {
+            result.append(" ").append(str);
+        }
+        return result.toString().trim();
+    }
+
+    public static String[] getNameAndSurname(String fullName) {
+
+        List<String> stringList = splitFullName(fullName);
+
+        String[] group = new String[2];
+
+        switch (stringList.size()) {
+            case 0:
+                group[0] = "---";
+                group[1] = "---";
+                return group;
+            case 1:
+                group[0] = stringList.get(0);
+                group[1] = "---";
+                return group;
+            default:
+                group[0] = stringList.get(0);
+                stringList.remove(0);
+                group[1] = listToString(stringList);
+                return group;
+        }
+
     }
 }
