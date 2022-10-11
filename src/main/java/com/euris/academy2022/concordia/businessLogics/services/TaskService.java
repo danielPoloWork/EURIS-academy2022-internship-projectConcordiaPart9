de60f7acb87c33d417aba8e8718ae1db8bc3a515
Daniel Polo <106583643+danielPoloWork@users.noreply.github.com>
2022-10-11 09:07:39 +0200

    Update TaskService.java
    Fixed imports to match folders, added methods to insert and update after fetching trello

--- src/main/java/com/euris/academy2022/concordia/businessLogics/services/TaskService.java
+++ src/main/java/com/euris/academy2022/concordia/businessLogics/services/TaskService.java
@@ -1,8 +1,8 @@
 package com.euris.academy2022.concordia.businessLogics.services;
 
-import com.euris.academy2022.concordia.dataPersistences.dataModels.Task;
-import com.euris.academy2022.concordia.dataPersistences.dataTransferObjects.TaskDto;
-import com.euris.academy2022.concordia.dataPersistences.dataTransferObjects.ResponseDto;
+import com.euris.academy2022.concordia.dataPersistences.models.Task;
+import com.euris.academy2022.concordia.dataPersistences.DTOs.TaskDto;
+import com.euris.academy2022.concordia.dataPersistences.DTOs.ResponseDto;
 import com.euris.academy2022.concordia.utils.enums.TaskPriority;
 import com.euris.academy2022.concordia.utils.enums.TaskStatus;
 
@@ -12,12 +12,16 @@ import java.util.List;
 public interface TaskService {
 
     ResponseDto<TaskDto> insert(Task task);
+    ResponseDto<TaskDto> insertFromTrello(Task task);
     ResponseDto<TaskDto> update(Task task);
+    ResponseDto<TaskDto> updateFromTrello(Task task);
     ResponseDto<TaskDto> deleteById(String id);
-    ResponseDto<TaskDto> getById(String id);
+    ResponseDto<TaskDto> getByIdTrelloTask(String id);
     ResponseDto<List<TaskDto>> getAll();
     ResponseDto<List<TaskDto>> getByPriority(TaskPriority priority);
     ResponseDto<List<TaskDto>> getByStatus(TaskStatus status);
     ResponseDto<List<TaskDto>> getByTitle(String title);
     ResponseDto<List<TaskDto>> getByDeadLine(LocalDateTime deadLine);
+    List<Task> findAllTasksByMemberUuid(String uuidMember);
+    void updateExpiringTasks();
 }
