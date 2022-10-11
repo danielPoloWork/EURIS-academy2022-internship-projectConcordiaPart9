d009d73ff4eee2663a2ecfd352a1eec55288da70
Daniel Polo <106583643+danielPoloWork@users.noreply.github.com>
2022-10-11 09:07:02 +0200

    Update MemberService.java
    Fixed imports to match folders, added methods to insert and update after fetching trello, fixed method names to match new model variables

--- src/main/java/com/euris/academy2022/concordia/businessLogics/services/MemberService.java
+++ src/main/java/com/euris/academy2022/concordia/businessLogics/services/MemberService.java
@@ -1,8 +1,8 @@
 package com.euris.academy2022.concordia.businessLogics.services;
 
-import com.euris.academy2022.concordia.dataPersistences.dataModels.Member;
-import com.euris.academy2022.concordia.dataPersistences.dataTransferObjects.MemberDto;
-import com.euris.academy2022.concordia.dataPersistences.dataTransferObjects.ResponseDto;
+import com.euris.academy2022.concordia.dataPersistences.models.Member;
+import com.euris.academy2022.concordia.dataPersistences.DTOs.MemberDto;
+import com.euris.academy2022.concordia.dataPersistences.DTOs.ResponseDto;
 import com.euris.academy2022.concordia.utils.enums.MemberRole;
 import org.springframework.stereotype.Service;
 
@@ -12,13 +12,15 @@ import java.util.List;
 public interface MemberService {
 
     ResponseDto<MemberDto> insert(Member member);
+    ResponseDto<MemberDto> insertFromTrello(Member member);
     ResponseDto<MemberDto> update(Member member);
+    ResponseDto<MemberDto> updateFromTrello(Member member);
     ResponseDto<MemberDto> removeByUuid(String uuid);
     ResponseDto<List<MemberDto>> getAll();
     ResponseDto<MemberDto> getByUuid(String uuid);
     ResponseDto<MemberDto> getByIdTrelloMember(String idTrelloMember);
     ResponseDto<MemberDto> getByUsername(String username);
     ResponseDto<List<MemberDto>> getByRole(MemberRole role);
-    ResponseDto<List<MemberDto>> getByName(String name);
-    ResponseDto<List<MemberDto>> getBySurname(String surname);
+    ResponseDto<List<MemberDto>> getByFirstName(String name);
+    ResponseDto<List<MemberDto>> getByLastName(String surname);
 }
