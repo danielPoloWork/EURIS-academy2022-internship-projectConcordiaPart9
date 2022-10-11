3bcb707f65e7a582f23df3a08be37c47df13e3e1
Daniel Polo <106583643+danielPoloWork@users.noreply.github.com>
2022-10-11 09:20:33 +0200

    Update JPAs
    Fixed imports to match new folder names, fixed query and methods to match new database structure, added some new custom query,

--- src/main/java/com/euris/academy2022/concordia/jpaRepositories/CommentJpaRepository.java
+++ src/main/java/com/euris/academy2022/concordia/jpaRepositories/CommentJpaRepository.java
@@ -1,8 +1,6 @@
 package com.euris.academy2022.concordia.jpaRepositories;
 
-import com.euris.academy2022.concordia.dataPersistences.dataModels.Comment;
-import com.euris.academy2022.concordia.dataPersistences.dataModels.Member;
-import com.euris.academy2022.concordia.dataPersistences.dataModels.Task;
+import com.euris.academy2022.concordia.dataPersistences.models.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -11,56 +9,81 @@ import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
-import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface CommentJpaRepository extends JpaRepository<Comment, String> {
 
-
     String INSERT_INTO_COMMENT =
-            "INSERT INTO Comment (Comment.uuid, Comment.text, Comment.lastUpdate, Comment.idTask, Comment.uuidMember) "
-                    + "VALUES (UUID(), :text, :lastUpdate, :idTask, :uuidMember)";
+            "INSERT INTO Comment (Comment.uuid, Comment.idTrelloComment, Comment.idTask, Comment.uuidMember, Comment.text, Comment.dateCreation, Comment.dateUpdate) " +
+            "VALUES (UUID(), :idTrelloComment, :idTask, :uuidMember, :text, :dateCreation, :dateUpdate)";
 
-    String UPDATE_COMMENT =
-            "UPDATE Comment "
-                    + "SET Comment.text = :text, Comment.lastUpdate = :lastUpdate "
-                    + "WHERE Comment.uuid = :uuid";
+    String UPDATE_COMMENT_BY_UUID =
+        "UPDATE Comment " +
+        "SET  Comment.text = :text, Comment.dateUpdate = :dateUpdate " +
+        "WHERE Comment.uuid = :uuid";
 
-    String DELETE_COMMENT =
-            "DELETE FROM Comment "
-            +"WHERE Comment.uuid = :uuid";
+    String UPDATE_COMMENT_BY_ID_TRELLO_COMMENT =
+            "UPDATE Comment " +
+            "SET Comment.idTrelloComment = :idTrelloComment, Comment.text = :text, Comment.dateUpdate = :dateUpdate " +
+            "WHERE Comment.uuid = :uuid";
+
+    String DELETE_COMMENT_BY_UUID =
+            "DELETE FROM Comment " +
+            "WHERE Comment.uuid = :uuid";
+
+    String DELETE_COMMENT_BY_ID_TRELLO_COMMENT =
+            "DELETE FROM Comment " +
+            "WHERE Comment.idTrelloComment = :idTrelloComment";
+
+    String SELECT_COMMENT_BY_ID_TRELLO_COMMENT =
+            "SELECT Comment.uuid, Comment.idTrelloComment, Comment.idTask, Comment.uuidMember, Comment.text, Comment.dateCreation, Comment.dateUpdate " +
+            "FROM Comment " +
+            "WHERE Comment.idTrelloComment = :idTrelloComment";
 
     @Modifying
     @Query(value = INSERT_INTO_COMMENT, nativeQuery = true)
     @Transactional
     Integer insert(
-            @Param("text") String text,
-            @Param("lastUpdate") LocalDateTime lastUpdate,
+            @Param("idTrelloComment") String idTrelloComment,
             @Param("idTask") String idTask,
-            @Param("uuidMember") String uuidMember);
+            @Param("uuidMember") String uuidMember,
+            @Param("text") String text,
+            @Param("dateCreation") LocalDateTime dateCreation,
+            @Param("dateUpdate") LocalDateTime dateUpdate);
 
     @Modifying
-    @Query(value = DELETE_COMMENT, nativeQuery = true)
-    @Transactional
-    Integer removeByUuid(
-            @Param("uuid") String uuid
-            );
-
-    @Modifying
-    @Query(value = UPDATE_COMMENT, nativeQuery = true)
+    @Query(value = UPDATE_COMMENT_BY_UUID, nativeQuery = true)
     @Transactional
     Integer update(
+            @Param("uuid") String uuid,
             @Param("text") String text,
-            @Param("lastUpdate") LocalDateTime lastUpdate,
-            @Param("uuid") String uuid
-    );
+            @Param("dateUpdate") LocalDateTime dateUpdate);
 
-    List<Comment> findByTask(String idTask);
+    @Modifying
+    @Query(value = UPDATE_COMMENT_BY_ID_TRELLO_COMMENT, nativeQuery = true)
+    @Transactional
+    Integer updateFromTrello(
+            @Param("uuid") String uuid,
+            @Param("idTrelloComment") String idTrelloComment,
+            @Param("text") String text,
+            @Param("dateUpdate") LocalDateTime dateUpdate);
 
-    List<Comment> findByMember(String uuidMember);
+    @Modifying
+    @Query(value = DELETE_COMMENT_BY_UUID, nativeQuery = true)
+    @Transactional
+    Integer removeByUuid(
+            @Param("uuid") String uuid);
 
-    String deleteByUuid(String uuid);
+    @Modifying
+    @Query(value = DELETE_COMMENT_BY_ID_TRELLO_COMMENT, nativeQuery = true)
+    @Transactional
+    Integer removeByIdTrelloComment(
+            @Param("idTrelloComment") String idTrelloComment);
+
+    @Query(value = SELECT_COMMENT_BY_ID_TRELLO_COMMENT, nativeQuery = true)
+    Optional<Comment> findByIdTrelloComment(
+            @Param("idTrelloComment") String idTrelloComment);
 
     Optional<Comment> findByUuid(String uuid);
 }
--- src/main/java/com/euris/academy2022/concordia/jpaRepositories/TaskJpaRepository.java
+++ src/main/java/com/euris/academy2022/concordia/jpaRepositories/TaskJpaRepository.java
@@ -1,6 +1,6 @@
 package com.euris.academy2022.concordia.jpaRepositories;
 
-import com.euris.academy2022.concordia.dataPersistences.dataModels.Task;
+import com.euris.academy2022.concordia.dataPersistences.models.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -9,36 +9,42 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 public interface TaskJpaRepository extends JpaRepository<Task, String> {
 
     String INSERT_INTO_TASK =
-            "INSERT INTO Task (Task.id, Task.title, Task.description, Task.priority, Task.status, Task.deadLine) "
-                    + "VALUES (:id, :title, :description, :priority, :status, :deadLine)";
+            "INSERT INTO Task (Task.id, Task.title, Task.description, Task.priority, Task.status, Task.deadLine, Task.dateCreation, Task.dateUpdate) "
+                    + "VALUES (:id, :title, :description, :priority, :status, :deadLine, :dateCreation, :dateUpdate)";
 
     String UPDATE_TASK =
             "UPDATE Task "
-                    + "SET Task.title = :title, Task.description = :description, Task.priority = :priority, Task.status = :status, Task.deadLine = :deadLine "
+                    + "SET Task.title = :title, Task.description = :description, Task.priority = :priority, Task.status = :status, Task.deadLine = :deadLine, Task.dateUpdate = :dateUpdate "
                     + "WHERE Task.id = :id";
 
     String DELETE_AUTHORIZATION =
             "DELETE FROM Task "
                     + "WHERE Task.id = :id";
 
+    String SELECT_BY_ID_TRELLO_TASK =
+            "SELECT Task.id, Task.title, Task.description, Task.priority, Task.status, Task.deadLine, Task.dateCreation, Task.dateUpdate " +
+            "FROM Task " +
+            "WHERE Task.id = :id";
+
     String SELECT_ALL_TASK_BY_MEMBER_UUID =
-            "SELECT Task.id, Task.title, Task.description, Task.priority, Task.status, Task.deadLine "
+            "SELECT Task.id, Task.title, Task.description, Task.priority, Task.status, Task.deadLine, Task.dateCreation, Task.dateUpdate "
                     + "FROM Member "
                     + "INNER JOIN Assignee ON Member.uuid = Assignee.uuidMember "
                     + "INNER JOIN Task ON Task.id = Assignee.idTask "
                     + "WHERE Member.uuid = :uuid";
 
     String SELECT_TASK_BY_PRIORITY =
-            "SELECT Task.id, Task.title, Task.description, Task.priority, Task.status, Task.deadLine "
+            "SELECT Task.id, Task.title, Task.description, Task.priority, Task.status, Task.deadLine, Task.dateCreation, Task.dateUpdate "
                     + "FROM Task "
                     + "WHERE Task.priority = :priority";
 
     String SELECT_TASK_BY_STATUS =
-            "SELECT Task.id, Task.title, Task.description, Task.priority, Task.status, Task.deadLine "
+            "SELECT Task.id, Task.title, Task.description, Task.priority, Task.status, Task.deadLine, Task.dateCreation, Task.dateUpdate "
                     + "FROM Task "
                     + "WHERE Task.status = :status";
 
@@ -51,7 +57,9 @@ public interface TaskJpaRepository extends JpaRepository<Task, String> {
             @Param("description") String description,
             @Param("priority") String priority,
             @Param("status") String status,
-            @Param("deadLine") LocalDateTime deadLine);
+            @Param("deadLine") LocalDateTime deadLine,
+            @Param("dateCreation") LocalDateTime dateCreation,
+            @Param("dateUpdate") LocalDateTime dateUpdate);
 
     @Modifying
     @Query(value = UPDATE_TASK, nativeQuery = true)
@@ -62,13 +70,16 @@ public interface TaskJpaRepository extends JpaRepository<Task, String> {
             @Param("description") String description,
             @Param("priority") String priority,
             @Param("status") String status,
-            @Param("deadLine") LocalDateTime deadLine);
+            @Param("deadLine") LocalDateTime deadLine,
+            @Param("dateUpdate") LocalDateTime dateUpdate);
 
     @Modifying
     @Query(value = DELETE_AUTHORIZATION, nativeQuery = true)
     @Transactional
     Integer removeById(
             @Param("id") String id);
+    @Query(value = SELECT_BY_ID_TRELLO_TASK, nativeQuery = true)
+    Optional<Task> findByIdTrelloTask(@Param("id") String id);
 
     @Query(value = SELECT_ALL_TASK_BY_MEMBER_UUID, nativeQuery = true)
     List<Task> findAllTasksByMemberUuid(@Param("uuid") String uuid);
