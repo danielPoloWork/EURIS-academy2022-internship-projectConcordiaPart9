c646948035c79d438690f3ab76b6e06301508833
Daniel Polo <106583643+danielPoloWork@users.noreply.github.com>
2022-10-11 09:07:11 +0200

    Update TabletService.java
    Fixed imports to match folders

--- src/main/java/com/euris/academy2022/concordia/businessLogics/services/TabletService.java
+++ src/main/java/com/euris/academy2022/concordia/businessLogics/services/TabletService.java
@@ -1,7 +1,7 @@
 package com.euris.academy2022.concordia.businessLogics.services;
 
-import com.euris.academy2022.concordia.dataPersistences.dataTransferObjects.ResponseDto;
-import com.euris.academy2022.concordia.dataPersistences.dataTransferObjects.TaskDto;
+import com.euris.academy2022.concordia.dataPersistences.DTOs.ResponseDto;
+import com.euris.academy2022.concordia.dataPersistences.DTOs.TaskDto;
 import com.euris.academy2022.concordia.utils.enums.TaskPriority;
 
 import java.util.List;
