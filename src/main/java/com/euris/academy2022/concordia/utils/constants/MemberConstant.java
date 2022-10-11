21fb250a4e0d5e58d91ee348708789b95c984961
Daniel Polo <106583643+danielPoloWork@users.noreply.github.com>
2022-10-11 09:15:24 +0200

    Create MemberConstant.java

---
+++ src/main/java/com/euris/academy2022/concordia/utils/constants/MemberConstant.java
@@ -0,0 +1,19 @@
+package com.euris.academy2022.concordia.utils.constants;
+
+public interface MemberConstant {
+    String TABLE_NAME = "Member";
+
+    String COLUMN_UUID = "uuid";
+    String COLUMN_ID_TRELLO_MEMBER = "idTrelloMember";
+    String COLUMN_ROLE = "role";
+    String COLUMN_USERNAME = "username";
+    String COLUMN_PASSWORD = "password";
+    String COLUMN_FIRST_NAME = "firstName";
+    String COLUMN_LAST_NAME = "lastName";
+    String COLUMN_DATE_CREATION = "dateCreation";
+    String COLUMN_DATE_UPDATE = "dateUpdate";
+
+    String PK = "pkMember";
+
+    String JOIN_MAPPED_BY = "member";
+}
