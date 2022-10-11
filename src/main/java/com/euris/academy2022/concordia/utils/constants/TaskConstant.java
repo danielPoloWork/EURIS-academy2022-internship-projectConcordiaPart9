3676283e69dd4d567dbfa45e96409c6db70f7c5e
Daniel Polo <106583643+danielPoloWork@users.noreply.github.com>
2022-10-11 09:15:28 +0200

    Create TaskConstant.java

---
+++ src/main/java/com/euris/academy2022/concordia/utils/constants/TaskConstant.java
@@ -0,0 +1,18 @@
+package com.euris.academy2022.concordia.utils.constants;
+
+public interface TaskConstant {
+    String TABLE_NAME = "Task";
+
+    String COLUMN_ID = "id";
+    String COLUMN_TITLE = "title";
+    String COLUMN_DESCRIPTION = "description";
+    String COLUMN_PRIORITY = "priority";
+    String COLUMN_STATUS = "status";
+    String COLUMN_DEADLINE = "deadline";
+    String COLUMN_DATE_CREATION = "dateCreation";
+    String COLUMN_DATE_UPDATE = "dateUpdate";
+
+    String PK = "pkTask";
+
+    String JOIN_MAPPED_BY = "task";
+}
