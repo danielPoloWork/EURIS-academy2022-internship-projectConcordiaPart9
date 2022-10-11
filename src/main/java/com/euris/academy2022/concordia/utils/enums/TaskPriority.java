51afac9bc2009ccdc0a095420bc2855c35b3d4cd
Daniel Polo <106583643+danielPoloWork@users.noreply.github.com>
2022-10-11 09:22:31 +0200

    Update TaskPriority.java
    Added a method to get the enum given trelloLabelId

--- src/main/java/com/euris/academy2022/concordia/utils/enums/TaskPriority.java
+++ src/main/java/com/euris/academy2022/concordia/utils/enums/TaskPriority.java
@@ -3,6 +3,11 @@ package com.euris.academy2022.concordia.utils.enums;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import javax.swing.text.html.Option;
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.stream.Stream;
+
 @RequiredArgsConstructor
 @Getter
 public enum TaskPriority {
@@ -41,4 +46,17 @@ public enum TaskPriority {
     private final String trelloLabelId;
     private final String trelloLabelName;
     private final String trelloLabelColor;
+    private static Stream<TaskPriority> stream() {
+        return Stream.of(TaskPriority.values());
+    }
+    public static TaskPriority getEnumByLabelId(String labelId) {
+//        return Arrays.stream(TaskPriority.values())
+//                .filter(val -> val.getTrelloLabelId().equals(id))
+//                .findFirst()
+//                .get();
+
+        return TaskPriority.stream()
+                .filter(val -> val.getTrelloLabelId().equals(labelId))
+                .findFirst().get();
+    }
 }
