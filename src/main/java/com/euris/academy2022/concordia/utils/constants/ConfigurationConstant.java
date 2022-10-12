f0c959a11df4cbab149ec4591aafe9703912f09e
Marcelloo1994 <114481672+Marcelloo1994@users.noreply.github.com>
2022-10-12 14:33:13 +0200

    Create ConfigurationConstant.java

---
+++ src/main/java/com/euris/academy2022/concordia/utils/constants/ConfigurationConstant.java
@@ -0,0 +1,15 @@
+package com.euris.academy2022.concordia.utils.constants;
+
+public interface ConfigurationConstant {
+
+  String TABLE_NAME = "CONFIGURATION";
+  String COLUMN_LABEL = "label";
+  String COLUMN_VALUE = "value";
+
+  String COLUMN_DATE_CREATION = "dateCreation";
+
+  String COLUMN_DATE_UPDATE = "dateUpdate";
+
+  String PK = "pkConfiguration";
+
+}
