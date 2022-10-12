21117004b0d5d63bf5f727401a95d11628d09474
4NTC <antonio.collinassi@gmail.com>
2022-10-12 09:35:03 +0200

    updateTest

--- src/test/java/com/euris/academy2022/concordia/jpaRepositories/TaskJpaRepositoryTest.java
+++ src/test/java/com/euris/academy2022/concordia/jpaRepositories/TaskJpaRepositoryTest.java
@@ -10,11 +10,16 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import javax.transaction.Transactional;
 
+import java.time.Duration;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalUnit;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -47,7 +52,6 @@ class TaskJpaRepositoryTest {
     @Test
     void insertTest() {
 
-
         taskJpaRepository.insert(
                 task.getId(),
                 task.getTitle(),
@@ -66,9 +70,41 @@ class TaskJpaRepositoryTest {
         Assertions.assertEquals(task.getDescription(), optionalTask.get().getDescription());
         Assertions.assertEquals(task.getPriority(), optionalTask.get().getPriority());
         Assertions.assertEquals(task.getStatus(), optionalTask.get().getStatus());
-        Assertions.assertEquals(task.getDeadLine(), optionalTask.get().getDeadLine());
-        Assertions.assertEquals(task.getDateCreation(), optionalTask.get().getDateCreation());
-        Assertions.assertEquals(task.getDateUpdate(), optionalTask.get().getDateUpdate());
+        Assertions.assertEquals(task.getDeadLine().truncatedTo(ChronoUnit.SECONDS), optionalTask.get().getDeadLine());
+        Assertions.assertEquals(task.getDateCreation().truncatedTo(ChronoUnit.SECONDS), optionalTask.get().getDateCreation());
+        Assertions.assertEquals(task.getDateUpdate().truncatedTo(ChronoUnit.SECONDS), optionalTask.get().getDateUpdate());
+
+    }
+    @Test
+    void updateTest() {
+
+        LocalDateTime updateDate = task.getDateUpdate().plusDays(5);
+
+        taskJpaRepository.insert(
+                task.getId(),
+                task.getTitle(),
+                task.getDescription(),
+                task.getPriority().getLabel(),
+                task.getStatus().getLabel(),
+                task.getDeadLine(),
+                task.getDateCreation(),
+                task.getDateUpdate()
+        );
+
+        taskJpaRepository.update(
+                task.getId(),
+                task.getTitle(),
+                task.getDescription(),
+                task.getPriority().getLabel(),
+                task.getStatus().getLabel(),
+                task.getDeadLine(),
+                updateDate
+        );
+
+        Optional<Task> optionalTask = taskJpaRepository.findById(task.getId());
+
+        Assertions.assertEquals(task.getId(), optionalTask.get().getId());
+        Assertions.assertEquals(updateDate.truncatedTo(ChronoUnit.SECONDS), optionalTask.get().getDateUpdate());
 
     }
 
