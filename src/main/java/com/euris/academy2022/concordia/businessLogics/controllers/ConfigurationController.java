1a030bd07377148f8d43f905343807b0f7e976ef
Marcelloo1994 <114481672+Marcelloo1994@users.noreply.github.com>
2022-10-12 14:39:55 +0200

    Create ConfigurationController.java

---
+++ src/main/java/com/euris/academy2022/concordia/businessLogics/controllers/ConfigurationController.java
@@ -0,0 +1,40 @@
+package com.euris.academy2022.concordia.businessLogics.controllers;
+
+import com.euris.academy2022.concordia.businessLogics.services.ConfigurationService;
+import com.euris.academy2022.concordia.dataPersistences.DTOs.ConfigurationDto;
+import com.euris.academy2022.concordia.dataPersistences.DTOs.ResponseDto;
+import com.euris.academy2022.concordia.dataPersistences.DTOs.requests.configurations.ConfigurationPostRequest;
+import com.euris.academy2022.concordia.dataPersistences.DTOs.requests.configurations.ConfigurationPutRequest;
+import com.euris.academy2022.concordia.dataPersistences.models.Configuration;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/controller")
+public class ConfigurationController {
+
+    private final ConfigurationService configurationService;
+
+    public ConfigurationController(ConfigurationService configurationService) {
+        this.configurationService = configurationService;
+    }
+
+    @PostMapping
+    public ResponseDto<ConfigurationDto> insert(@RequestBody ConfigurationPostRequest configurationDto) {
+        return configurationService.insert(configurationDto.toModel());
+    }
+
+    @PutMapping
+    public ResponseDto<ConfigurationDto> update(@RequestBody ConfigurationPutRequest configurationDto) {
+        return configurationService.update(configurationDto.toModel());
+    }
+
+    @DeleteMapping("/{label}")
+    public ResponseDto<ConfigurationDto> deleteByLabel(@PathVariable String label) {
+        return configurationService.deleteByLabel(label);
+    }
+
+    @GetMapping("/{label}")
+    public ResponseDto<ConfigurationDto> getByLabel(@PathVariable String label) {
+        return configurationService.getByLabel(label);
+    }
+}
