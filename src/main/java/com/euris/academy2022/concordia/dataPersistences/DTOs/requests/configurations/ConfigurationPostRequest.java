74d18e2d08dcc49b8d293cb1ea2d94a98469cbf2
Marcelloo1994 <114481672+Marcelloo1994@users.noreply.github.com>
2022-10-12 14:39:52 +0200

    Create put and post request

---
+++ src/main/java/com/euris/academy2022/concordia/dataPersistences/DTOs/requests/configurations/ConfigurationPostRequest.java
@@ -0,0 +1,33 @@
+package com.euris.academy2022.concordia.dataPersistences.DTOs.requests.configurations;
+
+
+import com.euris.academy2022.concordia.dataPersistences.archetypes.DtoArchetype;
+import com.euris.academy2022.concordia.dataPersistences.archetypes.ModelArchetype;
+import com.euris.academy2022.concordia.dataPersistences.models.Configuration;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class ConfigurationPostRequest implements DtoArchetype {
+
+    private String label;
+    private String value;
+    private LocalDateTime dateCreation;
+    private LocalDateTime dateUpdate;
+
+    @Override
+    public Configuration toModel() {
+        return Configuration.builder()
+                .label(this.label)
+                .value(this.value)
+                .dateCreation(this.dateCreation)
+                .dateUpdate(this.dateUpdate)
+                .build();
+    }
+}
