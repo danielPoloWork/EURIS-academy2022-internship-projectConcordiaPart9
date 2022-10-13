f07d18afbcf4dc4436652f54eb333610b135fcc4
Marcelloo1994 <114481672+Marcelloo1994@users.noreply.github.com>
2022-10-13 12:18:42 +0200

    Create ConnectionWindowConstant.java

---
+++ src/main/java/com/euris/academy2022/concordia/utils/constants/ConnectionWindowConstant.java
@@ -0,0 +1,11 @@
+package com.euris.academy2022.concordia.utils.constants;
+
+public interface ConnectionWindowConstant {
+
+    String TABLE_NAME = "ConnectionWindow";
+    String COLUMN_MONTH = "month";
+    String COLUMN_CRON = "cron";
+    String COLUMN_DATE_CREATION = "dateCreation";
+    String COLUMN_DATE_UPDATE = "dateUpdate";
+    String PK = "pkConnectionWindow";
+}
