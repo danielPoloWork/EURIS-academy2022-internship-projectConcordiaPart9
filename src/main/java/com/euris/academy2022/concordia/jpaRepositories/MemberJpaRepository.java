25191ed065ec516b7a8a974ccb71f5ee744e0731
4NTC <antonio.collinassi@gmail.com>
2022-10-13 12:24:30 +0200

    @Repository mancanti aggiunti

--- src/main/java/com/euris/academy2022/concordia/jpaRepositories/MemberJpaRepository.java
+++ src/main/java/com/euris/academy2022/concordia/jpaRepositories/MemberJpaRepository.java
@@ -13,7 +13,6 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
-
 @Repository
 public interface MemberJpaRepository extends JpaRepository<Member, String> {
     String INSERT_INTO_MEMBER =
