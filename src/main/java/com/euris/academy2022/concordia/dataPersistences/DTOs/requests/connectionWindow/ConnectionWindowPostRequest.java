4074e7da46e4d6d949027c68020ce57bf695a96b
Marcelloo1994 <114481672+Marcelloo1994@users.noreply.github.com>
2022-10-13 12:19:59 +0200

    Create ConnectionWindowPostRequest.java

---
+++ src/main/java/com/euris/academy2022/concordia/dataPersistences/DTOs/requests/connectionWindow/ConnectionWindowPostRequest.java
@@ -0,0 +1,31 @@
+package com.euris.academy2022.concordia.dataPersistences.DTOs.requests.connectionWindow;
+
+import com.euris.academy2022.concordia.dataPersistences.archetypes.DtoArchetype;
+import com.euris.academy2022.concordia.dataPersistences.archetypes.ModelArchetype;
+import com.euris.academy2022.concordia.dataPersistences.models.ConnectionWindow;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class ConnectionWindowPostRequest implements DtoArchetype {
+
+    private int month;
+    private String cron;
+    private LocalDateTime dateCreation;
+    private LocalDateTime dateUpdate;
+
+    @Override
+    public ConnectionWindow toModel() {
+        return ConnectionWindow.builder()
+                .month(this.month)
+                .cron(this.cron)
+                .dateCreation(this.dateCreation)
+                .dateUpdate(this.dateUpdate)
+                .build();
+    }
+}
