fd4e2a5b5df0f6f2ce4bf7f62ddf6e901a77477c
simoneov <106583727+simoneov@users.noreply.github.com>
2022-10-17 17:14:48 +0200

    modified email service

--- src/main/java/com/euris/academy2022/concordia/businessLogics/services/EmailService.java
+++ src/main/java/com/euris/academy2022/concordia/businessLogics/services/EmailService.java
@@ -3,5 +3,5 @@ package com.euris.academy2022.concordia.businessLogics.services;
 import com.euris.academy2022.concordia.dataPersistences.DTOs.EmailDto;
 
 public interface EmailService {
-    public void send(EmailDto email);
+    public void send();
 }
--- src/main/java/com/euris/academy2022/concordia/businessLogics/services/impls/EmailServiceImpl.java
+++ src/main/java/com/euris/academy2022/concordia/businessLogics/services/impls/EmailServiceImpl.java
@@ -1,5 +1,6 @@
 package com.euris.academy2022.concordia.businessLogics.services.impls;
 
+import com.euris.academy2022.concordia.businessLogics.services.ConfigurationService;
 import com.euris.academy2022.concordia.businessLogics.services.EmailService;
 import com.euris.academy2022.concordia.dataPersistences.DTOs.EmailDto;
 import org.springframework.stereotype.Service;
@@ -16,15 +17,23 @@ import java.util.Properties;
 public class EmailServiceImpl implements EmailService {
 
 
-    public void send(EmailDto email) {
+    private final ConfigurationService configurationService;
+
+    public EmailServiceImpl(ConfigurationService configurationService) {
+        this.configurationService = configurationService;
+    }
+
+    public void send() {
+
+EmailDto emailDto= EmailDto.builder()
+        .receiverAddress(configurationService.getByLabel("EMAIL_RECEIVER").getBody().getValue())
+        .senderAddress(configurationService.getByLabel("CONCORDIA_EMAIL_ADDRESS").getBody().getValue())
+        .senderUsername(configurationService.getByLabel("CONCORDIA_SENDER_EMAIL_APP_USERNAME").getBody().getValue())
+        .senderPassword(configurationService.getByLabel("CONCORDIA_SENDER_EMAIL_APP_PASSWORD").getBody().getValue())
+        .text(configurationService.getByLabel("EMAIL_TEXT").getBody().getValue())
+        .subject(configurationService.getByLabel("EMAIL_SUBJECT").getBody().getValue()).build();
 
 
-        String senderUsername = email.getSenderUsername();
-        String senderPassword = email.getSenderPassword();
-        String text = email.getText();
-        String subject = email.getSubject();
-        String senderAddress = email.getSenderAddress();
-        String receiverAddress = email.getReceiverAddress();
 
         Properties properties = new Properties();
         properties.put("mail.smtp.auth", "true");
@@ -34,21 +43,21 @@ public class EmailServiceImpl implements EmailService {
 
         Session session = Session.getInstance(properties, new javax.mail.Authenticator() {
             protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication(senderUsername, senderPassword);
+                return new PasswordAuthentication(emailDto.getSenderUsername(), emailDto.getSenderPassword());
             }
         });
 
         MimeMessage msg = new MimeMessage(session);
         try {
-            msg.setFrom(new InternetAddress(senderAddress));
-            msg.addRecipient(Message.RecipientType.TO, new InternetAddress(receiverAddress));
+            msg.setFrom(new InternetAddress(emailDto.getSenderAddress()));
+            msg.addRecipient(Message.RecipientType.TO, new InternetAddress(emailDto.getReceiverAddress()));
 
 
-            msg.setSubject(subject);
+            msg.setSubject(emailDto.getSubject());
 
             Multipart emailContent = new MimeMultipart();
             MimeBodyPart textBodyPart = new MimeBodyPart();
-            textBodyPart.setText(text);
+            textBodyPart.setText(emailDto.getText());
             MimeBodyPart pdfTasks = new MimeBodyPart();
             pdfTasks.attachFile("tasks.pdf");
             MimeBodyPart pdfPerformances = new MimeBodyPart();
